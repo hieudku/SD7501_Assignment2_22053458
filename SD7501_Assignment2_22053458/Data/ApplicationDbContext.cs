399834cb7a000fc84803b6835c2c13d7f2a34e76
Huba <hieu.dcu@gmail.com>
2025-04-06 00:50:28 +1300

    Add: DbContext, Category Model

---
+++ SD7501_Assignment2_22053458/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SD7501_Assignment2_22053458.Models;
+
+namespace SD7501_Assignment2_22053458.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; } = default!;
+    }
+}
---
+++ SD7501_Assignment2_22053458/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SD7501_Assignment2_22053458.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
