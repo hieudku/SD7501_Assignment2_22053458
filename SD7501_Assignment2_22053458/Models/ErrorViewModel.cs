496ba0a29c6b43c82dab32d6660afeb657ffd316
Huba <hieu.dcu@gmail.com>
2025-04-06 00:23:48 +1300

    Add project files.

---
+++ SD7501_Assignment2_22053458/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD7501_Assignment2_22053458.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
