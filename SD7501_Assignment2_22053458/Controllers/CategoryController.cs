6a6f4187eba55d6f5490b212a0113bac6bb8853c
Huba <hieu.dcu@gmail.com>
2025-04-07 12:43:23 +1200

    Add: Category index view

--- SD7501_Assignment2_22053458/Controllers/CategoryController.cs
+++ SD7501_Assignment2_22053458/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SD7501_Assignment2_22053458.Data;
+using SD7501_Assignment2_22053458.Models;
 
 namespace SD7501_Assignment2_22053458.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Category.ToList();
+            return View(objCategoryList);
         }
     }
 }
